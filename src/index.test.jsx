47bd3053eb97ea1675900b91b1a5d2aacd6cc0aa
akiokio <akio.xd@gmail.com>
2017-02-13 18:33:28 -0800

    Update coverage

--- src/index.test.jsx
+++
@@ -1,9 +0,0 @@
-/* global it, expect, describe, jest */
-
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './index';
-
-it('renders without crashing', () => {
-  shallow(<App />);
-});
